87e028a6f2656c9649e8a78a0d8b71d7caa69249
Ольга <281298@mai.ru>
2024-09-24 22:24:26 +0300

    Описание

---
+++ lesson.py
@@ -0,0 +1,11 @@
+print('Hello, world!')
+print(5)
+print(type(5))
+print(5/5)
+print(5//5) # // - целочисленное деление
+# % - остаток от деления
+# ** - возведение в степень
+# существуют изменяемые и неизменяемые типы данных, числа - это неизменяемые
+print('Hello'+' world')
+print(True, False) # logical tipe of data, named boolean
+print(5 > 10)
